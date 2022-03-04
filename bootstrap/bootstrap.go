6353c1c71626cf6aab58f2814d824c0caafd5f01
sj <362795776@qq.com>
2022-03-04 16:01:20 +0800

    配置方案的实现

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -1,5 +1,5 @@
 // Package bootstrap 处理程序初始化逻辑
-package bootstart
+package bootstrap
 
 import (
 	"gohub/routes"
@@ -15,10 +15,10 @@ func SetupRoute(router *gin.Engine) {
 	// 注册全局中间件
 	registerGlobalMiddleWare(router)
 
-	// 注册 API 路由
+	//  注册 API 路由
 	routes.RegisterAPIRoutes(router)
 
-	// 配置 404 路由
+	//  配置 404 路由
 	setup404Handler(router)
 }
 
@@ -36,12 +36,12 @@ func setup404Handler(router *gin.Engine) {
 		acceptString := c.Request.Header.Get("Accept")
 		if strings.Contains(acceptString, "text/html") {
 			// 如果是 HTML 的话
-			c.String(http.StatusNotFound, "页面返回404")
+			c.String(http.StatusNotFound, "页面返回 404")
 		} else {
-			// 默认返回 json
+			// 默认返回 JSON
 			c.JSON(http.StatusNotFound, gin.H{
 				"error_code":    404,
-				"error_message": "路由未定义，请确认 url 和请求方法是否正确",
+				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
 			})
 		}
 	})
