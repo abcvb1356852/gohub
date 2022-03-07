60209ee16c95186b5eddaf76db571857ca55c951
sj <362795776@qq.com>
2022-03-07 14:13:00 +0800

    验证请求

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -2,42 +2,51 @@
 package auth
 
 import (
-    "fmt"
-    v1 "gohub/app/http/controllers/api/v1"
-    "gohub/app/models/user"
-    "net/http"
+	"fmt"
+	v1 "gohub/app/http/controllers/api/v1"
+	"gohub/app/models/user"
+	"gohub/requests"
+	"net/http"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 // SignupController 注册控制器
 type SignupController struct {
-    v1.BaseAPIController
+	v1.BaseAPIController
 }
 
 // IsPhoneExist 检测手机号是否被注册
 func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 
-    // 请求对象
-    type PhoneExistRequest struct {
-        Phone string `json:"phone"`
-    }
-    request := PhoneExistRequest{}
+	// 请求对象
+	request := requests.SignupPhoneExistRequest{}
 
-    // 解析 JSON 请求
-    if err := c.ShouldBindJSON(&request); err != nil {
-        // 解析失败，返回 422 状态码和错误信息
-        c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-            "error": err.Error(),
-        })
-        // 打印错误信息
-        fmt.Println(err.Error())
-        // 出错了，中断请求
-        return
-    }
+	// 解析 JSON 请求
+	if err := c.ShouldBindJSON(&request); err != nil {
+		// 解析失败，返回 422 状态码和错误信息
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"error": err.Error(),
+		})
+		// 打印错误信息
+		fmt.Println(err.Error())
+		// 出错了，中断请求
+		return
+	}
 
-    //  检查数据库并返回响应
-    c.JSON(http.StatusOK, gin.H{
-        "exist": user.IsPhoneExist(request.Phone),
-    })
+	// 表单验证
+	errs := requests.ValidateSignupPhoneExist(&request, c)
+	// errs 返回长度等于 0 即通过，大于 0 即有错误发生
+	if len(errs) > 0 {
+		// 验证失败，返回 422 状态码和错误信息
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"errors": errs,
+		})
+		return
+	}
+
+	//  检查数据库并返回响应
+	c.JSON(http.StatusOK, gin.H{
+		"exist": user.IsPhoneExist(request.Phone),
+	})
 }
