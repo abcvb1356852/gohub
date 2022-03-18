e0135927ebd300d06c434d3cfd17de85fd437b2a
sj <362795776@qq.com>
2022-03-18 16:10:43 +0800

    图片验证码接口

--- pkg/captcha/captcha.go
+++ pkg/captcha/captcha.go
@@ -2,6 +2,7 @@
 package captcha
 
 import (
+	"gohub/pkg/app"
 	"gohub/pkg/config"
 	"gohub/pkg/redis"
 	"sync"
@@ -45,4 +46,21 @@ func NewCaptcha() *Captcha {
 	})
 
 	return internalCaptcha
+}
+
+// GenerateCaptcha 生成图片验证码
+func (c *Captcha) GenerateCaptcha() (id string, b64s string, err error) {
+	return c.Base64Captcha.Generate()
+}
+
+// VerifyCaptcha 验证验证码是否正确
+func (c *Captcha) VerifyCaptcha(id string, answer string) (match bool) {
+
+	// 方便本地和 API 自动测试
+	if !app.IsProduction() && id == config.GetString("captcha.testing_key") {
+		return true
+	}
+	// 第三个参数是验证后是否删除，我们选择 false
+	// 这样方便用户多次提交，防止表单提交错误需要多次输入图片验证码
+	return c.Base64Captcha.Verify(id, answer, false)
 }
