35ba14bc0e9a44cd8f08e60d470dbac8eb3fbfb8
sj <362795776@qq.com>
2022-03-18 10:56:11 +0800

    图片验证码库（captcha）

---
+++ pkg/captcha/captcha.go
@@ -0,0 +1,48 @@
+// Package captcha 处理图片验证码逻辑
+package captcha
+
+import (
+	"gohub/pkg/config"
+	"gohub/pkg/redis"
+	"sync"
+
+	"github.com/mojocn/base64Captcha"
+)
+
+type Captcha struct {
+	Base64Captcha *base64Captcha.Captcha
+}
+
+// once 确保 internalCaptcha 对象只初始化一次
+var once sync.Once
+
+// internalCaptcha 内部使用的 Captcha 对象
+var internalCaptcha *Captcha
+
+// NewCaptcha 单例模式获取
+func NewCaptcha() *Captcha {
+	once.Do(func() {
+		// 初始化 Captcha 对象
+		internalCaptcha = &Captcha{}
+
+		// 使用全局 Redis 对象，并配置存储 Key 的前缀
+		store := RedisStore{
+			RedisClient: redis.Redis,
+			KeyPrefix:   config.GetString("app.name") + ":captcha:",
+		}
+
+		// 配置 base64Captcha 驱动信息
+		driver := base64Captcha.NewDriverDigit(
+			config.GetInt("captcha.height"),      // 宽
+			config.GetInt("captcha.width"),       // 高
+			config.GetInt("captcha.length"),      // 长度
+			config.GetFloat64("captcha.maxskew"), // 数字的最大倾斜角度
+			config.GetInt("captcha.dotcount"),    // 图片背景里的混淆点数量
+		)
+
+		// 实例化 base64Captcha 并赋值给内部使用的 internalCaptcha 对象
+		internalCaptcha.Base64Captcha = base64Captcha.NewCaptcha(driver, &store)
+	})
+
+	return internalCaptcha
+}
