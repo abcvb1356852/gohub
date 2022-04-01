0372dba1644574c3b19c5dd56e683d7ed05c0830
sj <362795776@qq.com>
2022-04-01 15:42:52 +0800

    Auth 中间件

--- app/models/user/user_util.go
+++ app/models/user/user_util.go
@@ -29,3 +29,9 @@ func GetByMulti(loginID string) (userModel User) {
 	database.DB.Where("phone = ?", loginID).Or("email = ?", loginID).Or("name = ?", loginID).First(&userModel)
 	return
 }
+
+// Get 通过 ID 获取用户
+func Get(idstr string) (userModel User) {
+	database.DB.Where("id", idstr).First(&userModel)
+	return
+}
