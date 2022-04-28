0f2ec01b579ab419b10f2264d5dbcb4aad6d6c03
sj <362795776@qq.com>
2022-04-28 14:56:16 +0800

    用户列表分页

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -96,3 +96,9 @@ func deleteMySQLTbales() error {
 	DB.Exec("SET foreign_key_checks = 1;")
 	return nil
 }
+
+func TableName(obj interface{}) string {
+	stmt := &gorm.Statement{DB: DB}
+	stmt.Parse(obj)
+	return stmt.Schema.Table
+}
