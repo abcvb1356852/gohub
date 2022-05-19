90d9ba2dde01858c02d8fb970a56b07cac7fdcce
sj <362795776@qq.com>
2022-05-19 17:38:46 +0800

    编辑个人资料

--- routes/api.go
+++ routes/api.go
@@ -57,6 +57,7 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			usersGroup := v1.Group("/users")
 			{
 				usersGroup.GET("", uc.Index)
+				usersGroup.PUT("", middlewares.AuthJWT(), uc.UserUpdateProfile)
 			}
 			cgc := new(controllers.CategoriesController)
 			cgcGroup := v1.Group("/categories")
